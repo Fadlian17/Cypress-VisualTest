d6bf11f0a901f51e0bd3ea992cbc45412cf2f449
Fadlian17 <alfansyahfadlian@gmail.com>
2022-01-09 09:49:19 +0700

    visual testing part2

--- cypress/integration/visual-test.spec.js
+++ cypress/integration/visual-test.spec.js
@@ -1,7 +1,25 @@
+const pages = ["https://example.com/"]
+const sizes = ["iphone-6","ipad-2",[1200,800]]
+
+
 describe('Visual Regression Test', () => {
-    
-    it('Basic Visual Regression Test', () => {
-        cy.visit("http://example.com/")
-        cy.matchImageSnapshot()
+    sizes.forEach(size=>{
+        pages.forEach(page=>{
+            it(`Should match ${page} in resolution ${size}`, () => {
+                cy.setResolution(size)
+                cy.visit(page)
+                cy.matchImageSnapshot()
+            });
+        })
+    })
+});
+
+describe('Visual Single Elements Snapshot', () => {
+    it('Shoud match a single element snapshots', () => {
+        cy.visit('http://example.com')
+        cy.get('h1').matchImageSnapshot({
+            failureTreshold:10.0,
+            failureTresholdType:"pixels"
+        })
     });
 });
